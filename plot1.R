33404db4485e2b8f4e2e342de0038639c34856b9
Luca Cerone <luca.cerone@gmail.com>
2016-09-03 16:25:25 +0200

    Removed corrupted code

--- plot1.R
+++
@@ -1,7 +0,0 @@
-if (!exists("household")) source("import_data.R")
-
-hist(household$Global_active_power, col="red",
-    xlab="Global Active Power (kilowatts)", main ="Global Active Power")
-
-dev.copy(png,filename = "plot1.png", width = 480, height = 480)
-dev.off()
--- plot2.R
+++
@@ -1,7 +0,0 @@
-if (!exists("household")) source("import_data.R")
-
-plot(household$DateTime, household$Global_active_power,
-     xlab = "", ylab = "Global Active Power (kilowatts)", type="l")
-
-dev.copy(png,filename = "plot2.png", width = 480, height = 480)
-dev.off()
