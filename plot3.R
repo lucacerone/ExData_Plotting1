5f4a1b168504e5a17eaec751a24fa6f78ddf8cc2
Luca Cerone <luca.cerone@gmail.com>
2016-09-03 17:17:47 +0200

    Improved plot3 after finding better options

--- plot3.R
+++ plot3.R
@@ -1,12 +1,11 @@
 if (!exists("household")) source("import_data.R")
 
+png(filename = "plot3.png", width = 480, height = 480)
 plot(household$DateTime, household$Sub_metering_1,
      xlab = "", ylab = "Energy sub metering", type="l")
 lines(household$DateTime, household$Sub_metering_2, col = "red")
 lines(household$DateTime, household$Sub_metering_3, col = "blue")
 
-legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
-       col=c("black","red","blue"), lty=1, cex=.7, y.intersp=0.4)
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=1, lwd=3)
 
-dev.copy(png,filename = "plot3.png", width = 480, height = 480)
 dev.off()
